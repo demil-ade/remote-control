36e753869aec336638bb8bcdce872dd45615bd7b
Oluwasuyi Demilade <oluwademiladeoluwasuyi@gmail.com>
2023-04-18 17:53:04 +0100

    change

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,27 @@
 import {configureStore} from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import thunk from 'redux-thunk';
+
 import channelReducer from '../features/remote/channelSlice';
 import powerReducer from '../features/remote/powerSlice';
 import volumeReducer from '../features/remote/volumeSlice';
 
+const reducers = combineReducers({
+    channel: channelReducer,
+    turn: powerReducer,
+    volume: volumeReducer
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-    reducer: {
-        channel: channelReducer,
-        turn: powerReducer,
-        volume: volumeReducer
-    }
-})
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk],
+});
